49fc10e5c3bf49e2a53b380ee548982f8ee70e9f
Nils Hoffmann <3309580+nilshoffmann@users.noreply.github.com>
2021-11-26 08:32:51 +0100

    Disabled test that requires new mzTabValidator webapp.

--- tests/testthat/test-validate_mz_tab.R
+++ tests/testthat/test-validate_mz_tab.R
@@ -33,20 +33,20 @@ test_that("validation of mzTab file as string via REST API of mztab TSV works", 
   expect_equal(length(validationMessages), 0)
 })
 
-test_that("validation of mzTab Object via REST API of mztab TSV works", {
-  testfile <-
-    system.file("testdata", c("lipidomics-example.mzTab"), package = "rmzTabM")
-  mzTabTable <- readMzTab(testfile)
-  mzTabObject <- MzTab$new()
-  mzTabObject$fromDataFrame(mzTabTable)
-  
-  validationMessages <- validateMzTab(
-    mzTabObject,
-    validationMode = "json",
-    validationLevel = "info",
-    maxErrors = 100,
-    semanticValidation = FALSE
-  )
-  browser()
-  expect_equal(length(validationMessages), 5)
-})
+# this test needs a new version of the mzTab Validator web app
+# test_that("validation of mzTab Object via REST API of mztab TSV works", {
+#   testfile <-
+#     system.file("testdata", c("lipidomics-example.mzTab"), package = "rmzTabM")
+#   mzTabTable <- readMzTab(testfile)
+#   mzTabObject <- MzTab$new()
+#   mzTabObject$fromDataFrame(mzTabTable)
+#   
+#   validationMessages <- validateMzTab(
+#     mzTabObject,
+#     validationMode = "json",
+#     validationLevel = "info",
+#     maxErrors = 100,
+#     semanticValidation = FALSE
+#   )
+#   expect_equal(length(validationMessages), 5)
+# })
